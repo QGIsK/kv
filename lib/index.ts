2a3fa2dbc236971a7391affe092e524d608c44e4
Demian <Devaccdemiann@gmail.com>
2021-12-21 20:04:23 +0100

    feat: set

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,50 @@
-class Starter {}
+import {Schema, model, connect} from 'mongoose';
+import {isObject, isStringEmpty} from './helpers/utils';
 
-export {Starter, Starter as default};
+interface kv {
+    key: string;
+    value: string;
+}
+
+const kvSchema = new Schema<kv>({
+    key: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: String,
+        required: true,
+    },
+});
+
+const KeyModel = model<kv>('KV', kvSchema);
+
+class KV {
+    uri: string;
+    namespace: string;
+
+    constructor(uri: string, namespace: string) {
+        this.uri = isStringEmpty(uri) ? 'mongodb://localhost:27017/kv' : uri;
+        this.namespace = isStringEmpty(namespace) ? 'kv' : namespace;
+
+        connect(this.uri);
+    }
+
+    _createPrefix(key: string) {
+        return `${this.namespace}:${key}`;
+    }
+
+    async set(key: string, value: [String | Object]) {
+        const name = this._createPrefix(key);
+
+        const parsedValue = isObject(value) ? JSON.stringify(value) : value;
+
+        return await KeyModel.findOneAndUpdate(
+            {key: name},
+            {key: name, value: parsedValue},
+            {new: true, upsert: true, setDefaultsOnInsert: true},
+        );
+    }
+}
+
+export {KV};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,41 @@
 // @ts-nocheck
 
 import test from 'ava';
+import {KV} from '../dist/';
 
-test('hi', t => {
-    t.assert(1 === 1);
+const namespace = 'users';
+
+const key = 'key';
+const keySecondary = 'keySecondary';
+
+const value = 'value';
+const valueSecondary = 'valueSecondary';
+
+test('create prefix returns correct prefix', async t => {
+    const kv = new KV(undefined, namespace);
+
+    const prefix = kv._createPrefix(key);
+
+    t.is(prefix, `${namespace}:${key}`);
+});
+
+test('set value returns newly created value', async t => {
+    const kv = new KV();
+    const keyv = await kv.set(key, value);
+
+    t.true(keyv.key.includes(key));
+    t.is(keyv.value, value);
+});
+
+test('set value updates value that already exists', async t => {
+    const kv = new KV();
+    const keyv = await kv.set(key, value);
+
+    t.true(keyv.key.includes(key));
+    t.is(keyv.value, value);
+
+    const updatedKeyv = await kv.set(key, valueSecondary);
+
+    t.true(updatedKeyv.key.includes(key));
+    t.is(updatedKeyv.value, valueSecondary);
 });
